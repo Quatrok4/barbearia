e66127a5b3cec2e6674381a6dffc8671f476226a
Quatrok4 <lucaslou622@gmail.com>
2026-06-12 14:39:44 -0300

    ajuste no script para o back end

--- barbearia-frontend/script.js
+++ barbearia-frontend/script.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:3000";
+const API = "https://barbearia-backend.onrender.com";
 
 // ─── CADASTRO ────────────────────────────────────────────────────────────────
 async function cadastrar() {
